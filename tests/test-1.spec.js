1a6a2101fa8479da06a147bf97f2f425acb89e31
Sergei <s.egorov@gmail.com>
2025-10-27 23:05:57 +0300

    version1/test1 - добавлены комментарии в код

--- tests/test-1.spec.js
+++ tests/test-1.spec.js
@@ -13,11 +13,13 @@ function getUserCredantils (){
 }
 test.describe('Профиль пользователя',() =>{
 test.beforeEach ( async ({ page },testInfo) => {
+  // Условие для теста если не нужна регистрация пользователя
   if (testInfo.title.includes('Profile')){
        console.log('Пропускаем регистрацию для:', testInfo.title);
             return; // ← Выходим из beforeEach без регистрации
 
   }
+  // Генрация для регистрации данных
     const userEmail = faker.internet.email()
   const userPassword = faker.internet.password({length: 5})
   const userName = faker.person.firstName('male')
@@ -33,13 +35,14 @@ await registrationPage.register(userName,userEmail,userPassword)
     password:userPassword
 }
 });
+
 test ('Пользователь может изменить bio',async({page},testInfo)=>{
   const userBiogr = faker.person.bio()
 const userMainPage = new UserMainPage(page)
 await userMainPage.personSettingsPage()
 const profilePage = new ProfilePage(page)
 await profilePage.pfofileBiografia(userBiogr);
-await expect (profilePage.Biografia).toContainText(userBiogr)
+await expect (profilePage.Biografia).toContainText(userBiogr) // Проверяем что биография ,после нж. сохранения отображена на форме 
 })
 test('Проверить что пользователь может авторизоваться под созданной записью', async({page})=>{
 const email = userCredantils.email
@@ -50,7 +53,7 @@ const author = new Authorization(page);
 const userMainPage =new UserMainPage(page)
 await mainPage.btnLogin();
 await author.userLogin(email,password);
-await expect (page.getByText(name)).toBeVisible(name);
+await expect (page.getByText(name)).toBeVisible(name);// Проверяем что в профиле првильное имя 
 })
 
 })
